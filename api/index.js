1702875d637eec7c94774625a9e0e7e594871b50
unknown <ayotunde1900@gmail.com>
2025-04-03 10:43:23 +0100

    Backend setup in progress

--- api/index.js
+++ api/index.js
@@ -4,23 +4,38 @@ import { WebSocketServer } from "ws";
 import { db } from "./config/db.js";
 import { errorHandler } from "./config/error-handling.js";
 import AuthRouter from "./shared/routes/auth.js";
+
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
 const server = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
-server.get("/", (req, res, next) => {
-  console.log("hello world");
 
-  db.query("show tables", (err, result) => {
-    if (err) return next(err);
+// swagger ui setup
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CBT Application API",
+      version: "1.0.0",
+      description: "API documentation for my the cbt app",
+    },
+    servers: [
+      {
+        url: "http://localhost:" + PORT, // Change to your actual server URL
+      },
+    ],
+  },
+  apis: ["./*/routes/*.js"], // Path to the API routes
+};
 
-    console.log(result);
+const swaggerSpec = swaggerJSDoc(options);
 
-    return res.status(200).json({ tables: result });
-  });
-});
-
+// server middleware
+server.use("/api", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 server.use("/api/auth/", AuthRouter);
 
 server.use(errorHandler);
